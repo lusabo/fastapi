086aded397658b963d2a8c09d530e1748f61f5af
Luciano Borges <lucianosantosborges@gmail.com>
2025-02-08 21:23:57 +0000

    refactor: Adicionado a class Answer que não tinha ido no commit anterior

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .question import Question
 from .theme import Theme
 from .assessment import Assessment
-from .answers import Answer
+from .answer import Answer
 
 __all__ = ["Question", "Theme", "Assessment", "Answer"]
